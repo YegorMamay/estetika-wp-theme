1b10a051fd2276ecf40b471d5cbec36882f26668
Pavel <ponomarenko.pavel.web@gmail.com>
2019-11-16 20:20:45 +0200

    fixed style, block how to work

--- page-home.php
+++ page-home.php
@@ -9,41 +9,71 @@
 
 <?php get_template_part('loops/content', 'home'); ?>
 
-    <div class="row">
-        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-            <!-- активный класс для блока active -->
-            <div class="block-services active">
-                <div class="block-services__dots"></div>
-                <div class="block-services__description">
-                    Sed ut perspiciatis unde omnis iste
-                </div>
+    <div class="middle-block">
+        <div class="middle-block__item">
+            <div class="middle-block__icon-wrapper">
+                <img class="middle-block__icon" src="/wp-content/themes/estetika-wp-theme/assets/img/svg/request.svg" alt="icon">
             </div>
+            <div class="middle-block__description">Lorem ipsum dolor sit amet</div>
         </div>
-        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-            <div class="block-services">
-                <div class="block-services__dots"></div>
-                <div class="block-services__description">
-                    Sed ut perspiciatis unde omnis iste
-                </div>
+        <div class="middle-block__string"></div>
+        <div class="middle-block__item">
+            <div class="middle-block__icon-wrapper">
+                <img class="middle-block__icon" src="/wp-content/themes/estetika-wp-theme/assets/img/svg/manager.svg" alt="icon">
             </div>
+            <div class="middle-block__description">Lorem ipsum dolor sit amet</div>
         </div>
-        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-            <div class="block-services">
-                <div class="block-services__dots"></div>
-                <div class="block-services__description">
-                    Sed ut perspiciatis unde omnis iste
-                </div>
+        <div class="middle-block__string"></div>
+        <div class="middle-block__item">
+            <div class="middle-block__icon-wrapper">
+                <img class="middle-block__icon" src="/wp-content/themes/estetika-wp-theme/assets/img/svg/notification.svg" alt="icon">
             </div>
+            <div class="middle-block__description">Lorem ipsum dolor sit amet</div>
         </div>
-        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
-            <div class="block-services">
-                <div class="block-services__dots"></div>
-                <div class="block-services__description">
-                    Sed ut perspiciatis unde omnis iste
-                </div>
+        <div class="middle-block__string"></div>
+        <div class="middle-block__item">
+            <div class="middle-block__icon-wrapper">
+                <img class="middle-block__icon" src="/wp-content/themes/estetika-wp-theme/assets/img/svg/visit.svg" alt="icon">
             </div>
+            <div class="middle-block__description">Lorem ipsum dolor sit amet</div>
         </div>
     </div>
+
+<!--    <div class="row">-->
+<!--        <div class="col-12 col-sm-6 col-md-4 col-lg-3">-->
+<!--            активный класс для блока active -->
+<!--            <div class="block-services active">-->
+<!--                <div class="block-services__dots"></div>-->
+<!--                <div class="block-services__description">-->
+<!--                    Sed ut perspiciatis unde omnis iste-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--        <div class="col-12 col-sm-6 col-md-4 col-lg-3">-->
+<!--            <div class="block-services">-->
+<!--                <div class="block-services__dots"></div>-->
+<!--                <div class="block-services__description">-->
+<!--                    Sed ut perspiciatis unde omnis iste-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--        <div class="col-12 col-sm-6 col-md-4 col-lg-3">-->
+<!--            <div class="block-services">-->
+<!--                <div class="block-services__dots"></div>-->
+<!--                <div class="block-services__description">-->
+<!--                    Sed ut perspiciatis unde omnis iste-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--        <div class="col-12 col-sm-6 col-md-4 col-lg-3">-->
+<!--            <div class="block-services">-->
+<!--                <div class="block-services__dots"></div>-->
+<!--                <div class="block-services__description">-->
+<!--                    Sed ut perspiciatis unde omnis iste-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
 
 <button type="button" class="btn btn-primary btn-lg">кНопка 1</button>
 <button type="button" class="btn btn-primary btn-lg btn-arrow">
