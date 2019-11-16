5aff474d9b2db57b64c11e0f967f7d84a3fbb491
Pavel <ponomarenko.pavel.web@gmail.com>
2019-11-16 18:24:52 +0200

    fixed style

--- page-home.php
+++ page-home.php
@@ -8,6 +8,43 @@
 <div class="container">
 
 <?php get_template_part('loops/content', 'home'); ?>
+
+    <div class="row">
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+            <!-- активный класс для блока active -->
+            <div class="block-services active">
+                <div class="block-services__dots"></div>
+                <div class="block-services__description">
+                    Sed ut perspiciatis unde omnis iste
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+            <div class="block-services">
+                <div class="block-services__dots"></div>
+                <div class="block-services__description">
+                    Sed ut perspiciatis unde omnis iste
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+            <div class="block-services">
+                <div class="block-services__dots"></div>
+                <div class="block-services__description">
+                    Sed ut perspiciatis unde omnis iste
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+            <div class="block-services">
+                <div class="block-services__dots"></div>
+                <div class="block-services__description">
+                    Sed ut perspiciatis unde omnis iste
+                </div>
+            </div>
+        </div>
+    </div>
+
 <button type="button" class="btn btn-primary btn-lg">кНопка 1</button>
 <button type="button" class="btn btn-primary btn-lg btn-arrow">
     кНопка 2
